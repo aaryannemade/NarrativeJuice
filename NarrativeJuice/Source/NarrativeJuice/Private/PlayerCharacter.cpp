e7afafcd9081de642d159b2b8a1e490f9e9899f4
Aaryan Nemade <aaryan.nemade@ue-germany.de>
2022-10-29 13:50:56 +0200

    Converted current movement system to c++

--- NarrativeJuice/Source/NarrativeJuice/Private/PlayerCharacter.cpp
+++ NarrativeJuice/Source/NarrativeJuice/Private/PlayerCharacter.cpp
@@ -3,6 +3,8 @@
 
 #include "PlayerCharacter.h"
 
+#include "Components/CapsuleComponent.h"
+
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
@@ -23,6 +25,22 @@ void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	DeltaX = DeltaTime;
+
+	SetMovementInput(MoveX, MoveY);
+
+	SetDesiredMovementRotation(MoveX, MoveY, GetControlRotationYaw());
+
+	SetCharacterRotation();
+
+	SetMovementStartDirection();
+	
+	SetHadMovementInputLastFrame(MovementInput);
+
+	
+
+	//GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Yellow, FString::Printf(TEXT("Desired Movement Rotation: ") + DesiredMovementRotation.ToString()));
+
 }
 
 // Called to bind functionality to input
@@ -30,5 +48,124 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+
 }
+
+void APlayerCharacter::MoveForward(float value)
+{
+	//GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Yellow, TEXT("Move Forward is being called"));
+	
+	//Set up input move x value
+	MoveX = value;
+
+	//Add forward/back movement to character
+	const FVector Direction = FRotationMatrix(GetControlRotationYaw()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, MoveX);
+}
+
+void APlayerCharacter::MoveRight(float value)
+{
+	//GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Yellow, TEXT("Move Right is being called"));
+	
+	//set up input move y value
+	MoveY = value;
+
+	//Add right/left movement to character
+	const FVector Direction = FRotationMatrix(GetControlRotationYaw()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, MoveY);
+}
+
+FRotator APlayerCharacter::GetControlRotationYaw()
+{
+	const FRotator ControlRotation = FRotator(0, Controller->GetControlRotation().Yaw, 0);
+
+	return ControlRotation;
+}
+
+void APlayerCharacter::SetDesiredMovementRotation(float InputX, float InputY, FRotator ControlRotation)
+{
+	FVector MovementInputVector = FVector(InputX, -InputY, 0);
+
+	FRotator MovementInputRotator = MovementInputVector.Rotation();
+
+	DesiredMovementRotation = ControlRotation - MovementInputRotator;
+	
+}
+
+void APlayerCharacter::SetMovementInput(float InputX, float InputY)
+{
+	if(FMath::Abs(InputX) != 0.f || FMath::Abs(InputY) != 0.f)
+	{
+		MovementInput = true;
+	}
+	else
+	{
+		MovementInput = false;
+
+		SetMovementRotationMode(E_MovementRotationMode::RootMotionRotation);
+	}
+}
+
+void APlayerCharacter::SetHadMovementInputLastFrame(bool movementInput)
+{
+	HadMovementInputLastFrame = movementInput;
+}
+
+bool APlayerCharacter::StartedMovement()
+{
+	if(MovementInput && !HadMovementInputLastFrame)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("StartedMoving"));
+
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void APlayerCharacter::SetMovementStartDirection()
+{
+	if(StartedMovement())
+	{
+		MovementStartDirection = CalculateMovementStartDirection(MoveX, MoveY, GetCapsuleComponent()->GetComponentRotation(), GetControlRotationYaw());
+	}
+}
+
+float APlayerCharacter::CalculateMovementStartDirection(float InputX, float InputY, FRotator WorldRotation,
+	FRotator ControlRotation)
+{
+	//Calculate absolute input movement rotation
+	FVector MovementInputVector = FVector(InputX, -InputY, 0.f);
+	FRotator MovementInputRotation = MovementInputVector.Rotation();
+
+	//Calculate World Offset Rotation
+	FRotator CurrentRotation = ControlRotation - WorldRotation;
+
+	//Calculate Start Rotation
+	FRotator StartDirection = CurrentRotation - MovementInputRotation;
+	StartDirection = StartDirection.GetNormalized();
+
+	return StartDirection.Yaw;
+}
+
+void APlayerCharacter::SetMovementRotationMode(E_MovementRotationMode RotationMode)
+{
+	MovementRotationMode = RotationMode;
+}
+
+void APlayerCharacter::SetCharacterRotation()
+{
+	if(MovementInput && MovementRotationMode == E_MovementRotationMode::MovementInputRotation)
+	{
+		FRotator TargetRotation = FMath::Lerp(GetActorRotation(), DesiredMovementRotation, MovementRotationSpeed * DeltaX);
+		SetActorRotation(TargetRotation, ETeleportType::None);
+	}
+}
+
+
 
